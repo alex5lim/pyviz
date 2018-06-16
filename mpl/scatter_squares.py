ef1bc31453b1b7eb07b3be2c1a8af8f94a3b760a
Alexander Lim <alexander.halim+github@gmail.com>
2018-06-16 10:15:44 +0800

    move mpl files into a dir.

--- mpl/scatter_squares.py
+++ mpl/scatter_squares.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as pyplot
 
-x = list(range(1, 1001))
-y = [x**2 for x in x]
+x = list(range(1, 5001))
+y = [x**3 for x in x]
 
-pyplot.scatter(x, y, s=10)
+pyplot.scatter(x, y, s=40, c=y, cmap=pyplot.cm.Blues, edgecolor='none')
 
 # Set chart title and label
 pyplot.title("Square Numbers", fontsize=12)
@@ -14,6 +14,7 @@ pyplot.ylabel("Square of Value", fontsize=8)
 pyplot.tick_params(axis='both', which='major', labelsize=8)
 
 # Set the range for each axis
-# pyplot.axis([0, 1100, 0, 1100000])
+pyplot.axis([0, 6000, 0, 135000000000])
 
-pyplot.show()
+# Save the plot to a file named squares_plot.png 
+pyplot.savefig('squares_plot.png', bbox_inches='tight')
