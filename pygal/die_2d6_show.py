21306b2dada954a10f83722810750a0a8a9da0a5
Alexander Lim <alexander.halim+github@gmail.com>
2018-06-23 11:25:01 +0800

    Add two dice histogram.

---
+++ pygal/die_2d6_show.py
@@ -0,0 +1,63 @@
+import pygal
+from datetime import datetime
+
+
+from die import Die
+
+
+# Create two D6 dice
+d1 = Die()
+d2 = Die()
+
+# Roll the dice and save the results
+d1_results = []
+d2_results = []
+combined_results = []
+for value in range(1000):
+    d1_result = d1.roll()
+    d2_result = d2.roll()
+    d1_results.append(d1_result)
+    d2_results.append(d2_result)
+    combined_results.append(d1_result + d2_result)
+
+# Count frequency of the results
+d1_frequency = []
+d2_frequency = []
+combined_frequency = []
+
+for num in range(1, d1.num_sides + 1):
+    d1_frequency.append(d1_results.count(num))
+
+for num in range(1, d2.num_sides + 1):
+    d2_frequency.append(d2_results.count(num))
+
+for num in range(2, d1.num_sides + d2.num_sides + 1):
+    combined_frequency.append(combined_results.count(num))
+
+# Normalized frequency
+for num in range(d1.num_sides, d1.num_sides + d2.num_sides):
+    d1_frequency.append(None)
+
+for num in range(d2.num_sides, d1.num_sides + d2.num_sides):
+    d2_frequency.append(None)
+
+combined_frequency.insert(0, None)
+
+# Create histogram
+histogram = pygal.Bar()
+histogram.title = "Results of rolling dice 1000 times"
+histogram.x_labels = map(str, range(1, 13))
+histogram.x_title = "Result"
+histogram.y_title = "Frequency of result"
+histogram.add("D6_1", d1_frequency)
+histogram.add("D6_2", d2_frequency)
+histogram.add("Combined", combined_frequency)
+
+# Create filename
+filename = "dice_roll_result_"
+filename += datetime.now().strftime('%Y%m%d-%H%M%S')
+filename += ".svg"
+
+# Render histogram to file
+print("Render result to " + filename)
+histogram.render_to_file(filename)
