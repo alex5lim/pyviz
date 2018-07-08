25bf99ad30c7006480a768174c5178a6250a9f91
Alexander Lim <alexander.halim+github@gmail.com>
2018-07-08 17:28:46 +0800

    Add script to read Hacker News and output top news sorted with most comments.

---
+++ read_api/hn_submissions.py
@@ -0,0 +1,31 @@
+import requests
+from operator import itemgetter
+
+# Make an API call and store the response
+url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+r = requests.get(url)
+print("Status code: ", r.status_code)
+
+# Process information about each submission
+submission_ids = r.json()
+submission_dicts = []
+
+for id in submission_ids[:30]:
+    url = "https://hacker-news.firebaseio.com/v0/item/" + str(id) + ".json"
+    r = requests.get(url)
+    print(r.status_code)
+    submission_item = r.json()
+    submission_dict = {
+       'title': submission_item['title'],
+       'link': submission_item['url'],
+       'comments': submission_item.get('descendants', 0)
+       }
+    submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
+                          reverse=True)
+
+for item in submission_dicts:
+    print("\nTitle:", item['title'])
+    print("Discussion link:", item['link'])
+    print("Coments: ", item['comments'])
